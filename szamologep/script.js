940be026cda8b01413734a8376c67886a895180a
BarcziBence <94312075+BarcziBence@users.noreply.github.com>
2024-11-12 14:10:18 +0100

    Add files via upload

--- szamologep/script.js
+++ szamologep/script.js
@@ -1,12 +1,54 @@
 
+var number = 0;
+var oper = 0;
 
-var 
+var numbers = [];
+var operations = [];
+
 
 
 function NumButton(num){
-
+    if(document.getElementById('main-disp').innerHTML == 0){
+        document.getElementById('main-disp').innerHTML = "";
+    }
+    number = document.getElementById('main-disp').innerHTML += num;
+    
 }
 
 function FunctionalButton(func){
+    switch(func){
+        case 'SetOpposite':
+            number = number * -1;
+            document.getElementById('main-disp').innerHTML = number;
+            break;
+        case 'Square':
+            number = number * number;
+            document.getElementById('main-disp').innerHTML = number;
+            break;
+    }
+}
 
+function SetSideDisplay(operation){
+    document.getElementById('side-disp').innerHTML += (' ' + number + ' ' + operation);
+    numbers[numbers.length] = number
+    operations[operations.length] = operation
+    number = 0;
+    document.getElementById('main-disp').innerHTML = number;
+
+}
+
+function Calculate(){
+    document.getElementById('side-disp').innerHTML += (' ' + '=');
+    
+}
+
+function ResetCalculator(){
+    number = 0;
+    document.getElementById('main-disp').innerHTML = number;
+    document.getElementById('side-disp').innerHTML = "";
+}
+
+function SetToZero(){
+    number = 0;
+    document.getElementById('main-disp').innerHTML = number;
 }
